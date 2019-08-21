0195b798d15928ad3960c07ff4968a83cacef553
Hyumiris <isnotyourid@gmx.de>
2019-08-21 20:28:19 +0200

    some format cleanup

--- src/app/services/stl.service.ts
+++ src/app/services/stl.service.ts
@@ -28,8 +28,6 @@ export class StlService {
 				const secondVertexOffset = 24;
 				const thirdVertexOffset = 36;
 
-				const color = vec3.fromValues(.5, .5, .5);
-
 				for (let i = 0; i < numberPrimitives; ++i) {
 					const primitiveOffset = basicOffset + (i * sizePerPrimitive);
 
@@ -49,22 +47,24 @@ export class StlService {
 						dataView.getFloat32(primitiveOffset + thirdVertexOffset + 8, true)
 					);
 
-					const normalCalculated = vec3.create();
-					const diff1 = vec3.create();
-					const diff2 = vec3.create();
-					vec3.cross(normalCalculated, vec3.sub(diff1, vertex0, vertex1), vec3.sub(diff2, vertex0, vertex2));
-					vec3.normalize(normalCalculated, normalCalculated);
-
 					const normal = vec3.fromValues(
 						dataView.getFloat32(primitiveOffset + normalOffset + 0, true),
 						dataView.getFloat32(primitiveOffset + normalOffset + 4, true),
 						dataView.getFloat32(primitiveOffset + normalOffset + 8, true)
 					);
-					vec3.normalize(normal, normal);
+					if (vec3.length(normal) === 0) {
+						const diff1 = vec3.create();
+						const diff2 = vec3.create();
+						vec3.cross(normal, vec3.sub(diff1, vertex0, vertex1), vec3.sub(diff2, vertex0, vertex2));
+						vec3.normalize(normal, normal);
+					}
+					if (Math.abs(vec3.length(normal) - 1) > 0.0001) {
+						vec3.normalize(normal, normal);
+					}
 
-					vertices[(i * 3) + 0] = { position: vertex0, normal, color, normalCalculated };
-					vertices[(i * 3) + 1] = { position: vertex1, normal, color, normalCalculated };
-					vertices[(i * 3) + 2] = { position: vertex2, normal, color, normalCalculated };
+					vertices[(i * 3) + 0] = { position: vertex0, normal };
+					vertices[(i * 3) + 1] = { position: vertex1, normal };
+					vertices[(i * 3) + 2] = { position: vertex2, normal };
 				}
 				return vertices;
 			})
--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -4,10 +4,11 @@ export type TypedArray = Int8Array | Int16Array | Int32Array
 	| Uint8ClampedArray | Uint8Array | Uint16Array | Uint32Array
 	| Float32Array | Float64Array;
 
+
+export const SIZE_FLOAT = 4;
+export const SIZE_VERTEX_DATA = 6;
 export interface IVertexData {
 	position: vec3;
 	/** normalized normal vector */
 	normal: vec3;
-	normalCalculated?: vec3;
-	color: vec3;
 }
