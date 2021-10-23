5c5abdc6e5717754cdf684b78185af5e3d778679
Hyumiris <isnotyourid@gmx.de>
2021-10-23 09:10:17 +0400

    type shuffling

--- src/app/ModelLoader/ModelLoader.types.ts
+++ src/app/ModelLoader/ModelLoader.types.ts
@@ -9,10 +9,30 @@ export interface IModelData {
 	Z: { min: number, max: number };
 }
 
+export interface IFace {
+	/** 0-indices for v */
+	position: number[];
+	/** 0-indices for vt */
+	texCoords: number[];
+	/** 0-indices for vn */
+	normal: number[];
+	/** name of the material associated with this face */
+	material: string;
+}
+
+export interface IObjModel {
+	v: vec3[];
+	vn: vec3[];
+	vt: vec2[];
+	faces: IFace[];
+	materials: IMaterialGroup;
+}
+
 export interface IMaterial {
 	ambient: vec3;
 	diffuse: vec3;
 	specular: vec3;
+	/** 0 - 1000 */
 	specular_exp: number;
 	color_map?: string;
 	bump_map?: string;
--- src/app/ModelLoader/obj.loader.ts
+++ src/app/ModelLoader/obj.loader.ts
@@ -2,7 +2,7 @@ import { vec2, vec3 } from 'gl-matrix';
 import { forkJoin, from, Observable } from 'rxjs';
 import { defaultIfEmpty, filter, map, mergeAll, mergeMap, reduce, tap } from 'rxjs/operators';
 import { computeRelativePath, loadTextFile } from '../helper/fileLoader';
-import { IMaterialGroup, IModelData } from './ModelLoader.types';
+import { IFace, IMaterialGroup, IModelData, IObjModel } from './ModelLoader.types';
 import { loadMaterials } from './mtl.loader';
 
 // ==============================================================================================
@@ -12,26 +12,11 @@ import { loadMaterials } from './mtl.loader';
 const lineTypes = ['v', 'vn', 'vt', 'f', 'mtllib', 'usemtl'] as const;
 type LineType = (typeof lineTypes)[number];
 
-interface IFace {
-	/** 0-indeces for v in the IAccumulatingObj */
-	position: number[];
-	/** 0-indeces for vt in the IAccumulatingObj */
-	texCoords: number[];
-	/** 0-indeces for vn in the IAccumulatingObj */
-	normal: number[];
-	usemtl: string;
-}
-
-interface IAccumulatingObj {
+type IObjBuffer = IObjModel & {
 	path: string;
-	v: vec3[];
-	vn: vec3[];
-	vt: vec2[];
-	f: IFace[];
-	materials: IMaterialGroup;
-	usemtl: string;
+	material: string;
 	mtllibs: Observable<IMaterialGroup>[];
-}
+};
 
 // ==============================================================================================
 // ================================== SUPPORT STRUCTURES ========================================
@@ -39,7 +24,7 @@ interface IAccumulatingObj {
 
 const lineSplitRe = /([^\s]+)/g;
 
-const getAccObj = (path: string): IAccumulatingObj => ({ path, v: [], vt: [], vn: [], f: [], materials: {}, usemtl: '', mtllibs: [] });
+const getAccObj = (path: string): IObjBuffer => ({ path, v: [], vt: [], vn: [], faces: [], materials: {}, material: '', mtllibs: [] });
 
 const baseModelData = (): IModelData => ({
 	position: [] as vec3[],
@@ -50,13 +35,13 @@ const baseModelData = (): IModelData => ({
 	Z: { min: Infinity, max: -Infinity }
 });
 
-const normalizeFaceIndex = (idx: number, type: 'v' | 'vt' | 'vn', acc: IAccumulatingObj) => {
+const normalizeFaceIndex = (idx: number, type: 'v' | 'vt' | 'vn', acc: IObjBuffer) => {
 	return idx > 0
 		? idx - 1
 		: acc[type].length + idx;
 };
 
-const lineHandler: { [type in LineType]: (acc: IAccumulatingObj, parts: string[]) => any } = {
+const lineHandler: { [type in LineType]: (acc: IObjBuffer, parts: string[]) => any } = {
 	// List of geometric vertices, `x y z [w]`, w is optional and defaults to 1.0.
 	v: (acc, parts) => acc.v.push(vec3.fromValues.apply(null, parts.map(parseFloat)) as vec3),
 	// List of texture coordinates, `u [v [w]]`, these will vary between 0 and 1. v, w are optional and default to 0.
@@ -65,17 +50,17 @@ const lineHandler: { [type in LineType]: (acc: IAccumulatingObj, parts: string[]
 	vn: (acc, parts) => acc.vn.push(vec3.fromValues.apply(null, parts.map(parseFloat)) as vec3),
 	// roughly `v1/vt1/vn1 v2/vt2/vn2 v3/vt3/vn3` with 1 indeces
 	f: (acc, parts) => {
-		acc.f.push(parts.map(s => s.split('/').map(parseFloat)).reduce((face, [v, vt, vn]) => {
+		acc.faces.push(parts.map(s => s.split('/').map(parseFloat)).reduce((face, [v, vt, vn]) => {
 			face.position.push(normalizeFaceIndex(v, 'v', acc));
 			face.texCoords.push(normalizeFaceIndex(vt, 'vt', acc));
 			face.normal.push(normalizeFaceIndex(vn, 'vn', acc));
 			return face;
-		}, { position: [], texCoords: [], normal: [], usemtl: acc.usemtl } as IFace));
+		}, { position: [], texCoords: [], normal: [], material: acc.material } as IFace));
 	},
 	// mtl file to be used to resolve materials
 	mtllib: (acc, [path]) => acc.mtllibs.push(loadMaterials(computeRelativePath(acc.path, path))),
 	// which material to use for the following faces
-	usemtl: (acc, [name]) => acc.usemtl = name
+	usemtl: (acc, [name]) => acc.material = name
 };
 
 /** takes a face and returns an array of triangular faces which are together equivalent to the original face */
@@ -84,21 +69,17 @@ const asTriangleFaces: ((f: IFace) => IFace[])[] = [
 	() => { throw new Error('not enough vertices'); },
 	() => { throw new Error('can\'t handle 2 component vertices'); },
 	(input) => [input],
-	(input) => {
-		const retVal: IFace[] = [
-			{ position: [], texCoords: [], normal: [], usemtl: input.usemtl },
-			{ position: [], texCoords: [], normal: [], usemtl: input.usemtl }
-		];
-		retVal[0].position = [input.position[0], input.position[1], input.position[2]];
-		retVal[0].texCoords = [input.texCoords[0], input.texCoords[1], input.texCoords[2]];
-		retVal[0].normal = [input.normal[0], input.normal[1], input.normal[2]];
-
-		retVal[1].position = [input.position[2], input.position[3], input.position[0]];
-		retVal[1].texCoords = [input.texCoords[2], input.texCoords[3], input.texCoords[0]];
-		retVal[1].normal = [input.normal[2], input.normal[3], input.normal[0]];
-
-		return retVal;
-	}
+	(input) => [{
+		position: [input.position[0], input.position[1], input.position[2]],
+		texCoords: [input.texCoords[0], input.texCoords[1], input.texCoords[2]],
+		normal: [input.normal[0], input.normal[1], input.normal[2]],
+		material: input.material
+	}, {
+		position: [input.position[2], input.position[3], input.position[0]],
+		texCoords: [input.texCoords[2], input.texCoords[3], input.texCoords[0]],
+		normal: [input.normal[2], input.normal[3], input.normal[0]],
+		material: input.material
+	}]
 ];
 
 // ==============================================================================================
@@ -126,7 +107,7 @@ export function loadObjModel(path: string) {
 			map(() => acc)
 		)),
 		// parse each face
-		mergeMap(acc => from(acc.f).pipe(
+		mergeMap(acc => from(acc.faces).pipe(
 			// split non triangle faces into triangles
 			mergeMap((face) => asTriangleFaces[face.position.length](face)),
 			// put each face into the final model data
